c03d45cf9ff43fa26500d45513d6b69159f5328f
yassshhhh22 <mryash7969@gmail.com>
2025-07-13 22:18:08 +0530

    refactor: simplify MongoDB connection handling with asyncHandler

--- server/config/db.js
+++ server/config/db.js
@@ -1,16 +1,12 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import asyncHandler from "express-async-handler";
 
-const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('MongoDB connected');
-    } catch (err) {
-        console.error('MongoDB connection error:', err.message);
-        process.exit(1);
-    }
-};
+const connectDB = asyncHandler(async () => {
+  await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log("MongoDB connected");
+});
 
 export default connectDB;
