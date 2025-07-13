977a07e7fa7f9898a2beb99c5d23023af817111b
yassshhhh22 <mryash7969@gmail.com>
2025-07-13 21:48:50 +0530

    refactor: remove Firebase authentication middleware and related configurations

--- server/config/firebaseAdmin.js
+++
@@ -1,8 +0,0 @@
-import admin from 'firebase-admin';
-import serviceAccount from './firebase-service-account-key.json';
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-
-export default admin;
