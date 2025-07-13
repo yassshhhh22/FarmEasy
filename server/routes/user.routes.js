fcfab512c118c5430e9e4a09140cc47f7eac9965
yassshhhh22 <mryash7969@gmail.com>
2025-07-13 23:16:12 +0530

    done for the day

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -4,6 +4,6 @@ import {createProfile,loginuser} from "../controllers/user.controller.js";
 const router = express.Router();
 
 router.route("/signup").post(createProfile);
-router.route("/login").post(loginuser); // Assuming signin uses the same controller for now
+router.route("/login").post(loginuser); 
 
 export default router;
