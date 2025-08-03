7fcd994f3db005c9b603bb195fd57504c9aac65e
yassshhhh22 <mryash7969@gmail.com>
2025-08-03 20:25:31 +0530

    fix: Correct import path casing for ApiError utility

--- server/middleware/auth.middleware.js
+++ server/middleware/auth.middleware.js
@@ -1,4 +1,4 @@
-import { ApiError } from "../utils/apiError.js";
+import { ApiError } from "../utils/ApiError.js";
 import asynchandler from "express-async-handler";
 import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
